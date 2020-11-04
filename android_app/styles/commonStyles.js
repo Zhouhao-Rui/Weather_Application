d62b4113ddfe5719ad9ff683a843c2896a3063f4
Zhouhao Rui <976667412@qq.com>
2020-11-04 16:26:36 +0000

    Feature: Add common util function

---
+++ android_app/styles/commonStyles.js
@@ -0,0 +1,12 @@
+import {
+  StyleSheet
+} from 'react-native'
+
+const commonStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#efeff3'
+  }
+})
+
+export default commonStyles
---
+++ android_app/utils/extension.js
@@ -0,0 +1,23 @@
+// 封装数据过滤
+function filterFieldData(courseData, field, doSlice) {
+  const _data = courseData.filter((item, index) => {
+    if (filed == 'all') {
+      return true
+    }
+
+    return item.field === field;
+  })
+
+  return doSlice ? _data.slice(0, 4) : _data
+}
+
+// 封装跳转页面
+function directToPage (navigation, pageName, params) {
+  return function() {
+    navigation.navigate(pageName, params)
+  }
+}
+
+export {
+  filterFieldData
+}
