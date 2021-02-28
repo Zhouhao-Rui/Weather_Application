9e664218a80c15f43fff85c52e1d80de7c87074e
Zhouhao Rui <976667412@qq.com>
2021-02-28 22:40:21 +0000

    Modify gitignore

--- android_app/pages/NewsPage.js
+++ android_app/pages/NewsPage.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-const ListPage = () => {
+const NewsPage = () => {
   return (
     <View>
-      <Text>List Page</Text>
+      
     </View>
   )
 }
 
-export default ListPage
+export default NewsPage
